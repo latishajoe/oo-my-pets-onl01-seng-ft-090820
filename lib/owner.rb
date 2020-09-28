8d0eec40bd91ffe157f99561020ef6e63bfcecfa
Latisha Joe <latisha_joe@yahoo.com>
2020-09-28 15:55:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,29 @@
 class Owner
   attr_reader :name, :species
 
+  @@all = []
+  @@count = 0
+
   def initialize(name, species)
     @name = name
     @species = species
-    say_species()
+    @@all << self 
+    @@count += 1
   end
+
+  def self.all
+    @@all
+  end 
+
+  def say_species(species)
+    say "I am #{species}"
+  end 
+
+  def count.all 
+    @@count 
+  end 
+    
+    
 
 
 
