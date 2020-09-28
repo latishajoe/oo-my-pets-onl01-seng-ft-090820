4d12b45bd1be94b0630ca7d0d76e897d397df664
Latisha Joe <latisha_joe@yahoo.com>
2020-09-28 17:06:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -37,11 +37,11 @@ class Owner
   end
 
   def buy_cat(name)
-    Cats.new(name, self)
+    Cat.new(name, self)
   end
 
   def buy_cat(name)
-    Dogs.new(name, self)
+    Dog.new(name, self)
   end
 
 
